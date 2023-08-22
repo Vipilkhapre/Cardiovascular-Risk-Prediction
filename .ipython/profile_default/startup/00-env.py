05f0ccf344bb4d8f7291c759bc59fb4ceb4d96f0
Vipil Khapre <vipilkhapre3@gmail.com>
2023-08-22 17:24:21 +0000

    first commit

---
+++ .ipython/profile_default/startup/00-env.py
@@ -0,0 +1,14 @@
+# *WARNING* Do not change this file as it may break your notebooks
+
+import os
+os.environ['HOME'] = '/home/c813ed0f-30fe-4c92-adf8-adb7b9ede5b7'
+os.environ['LC_CTYPE'] = 'en_US.utf-8'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+os.environ['TERM'] = 'xterm-256color'
+os.environ['PYTHONSTARTUP'] = '/home/c813ed0f-30fe-4c92-adf8-adb7b9ede5b7/.pythonstartup.py'
+os.environ['PYTHONANYWHERE_DOMAIN'] = 'anacondaapps.cloud'
+os.environ['PYTHONANYWHERE_SITE'] = 'nb.anaconda.cloud'
+os.environ['http_proxy'] = 'http://proxy.server:3128'
+os.environ['https_proxy'] = 'http://proxy.server:3128'
+os.environ['no_proxy'] = 'localhost,127.0.0.1,localaddress,.localdomain.com,/var/run/docker.sock'
+os.environ['API_TOKEN'] = '7d7c683678dd7f3752532b2074998d3adaf0a9ac'
